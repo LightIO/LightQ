6a389c6df66c17393f73b39f94ce2a0ab1100909
Rohit Joshi <rohit.c.joshi@gmail.com>
2015-03-29 15:35:05 -0400

    deleting log files

---
+++ build/Release/GNU-MacOSX/main.o.d
@@ -0,0 +1,139 @@
+build/Release/GNU-MacOSX/main.o: main.cpp include/log.h \
+ include/thirdparty/spdlog/spdlog.h include/thirdparty/spdlog/common.h \
+ include/thirdparty/spdlog/logger.h \
+ include/thirdparty/spdlog/sinks/base_sink.h \
+ include/thirdparty/spdlog/sinks/./sink.h \
+ include/thirdparty/spdlog/sinks/./../details/log_msg.h \
+ include/thirdparty/spdlog/sinks/./../details/./format.h \
+ include/thirdparty/spdlog/sinks/../formatter.h \
+ include/thirdparty/spdlog/sinks/../details/pattern_formatter_impl.h \
+ include/thirdparty/spdlog/sinks/../details/./os.h \
+ include/thirdparty/spdlog/./details/logger_impl.h \
+ include/thirdparty/spdlog/./details/./line_logger.h \
+ include/thirdparty/spdlog/details/spdlog_impl.h \
+ include/thirdparty/spdlog/details/registry.h \
+ include/thirdparty/spdlog/details/../async_logger.h \
+ include/thirdparty/spdlog/details/.././details/async_logger_impl.h \
+ include/thirdparty/spdlog/details/.././details/./async_log_helper.h \
+ include/thirdparty/spdlog/details/.././details/././mpmc_bounded_q.h \
+ include/thirdparty/spdlog/details/.././details/././format.h \
+ include/thirdparty/spdlog/details/../sinks/file_sinks.h \
+ include/thirdparty/spdlog/details/../sinks/../details/null_mutex.h \
+ include/thirdparty/spdlog/details/../sinks/../details/file_helper.h \
+ include/thirdparty/spdlog/details/../sinks/../details/format.h \
+ include/thirdparty/spdlog/details/../sinks/stdout_sinks.h \
+ include/thirdparty/spdlog/details/../sinks/./ostream_sink.h \
+ include/thirdparty/spdlog/details/../sinks/syslog_sink.h \
+ include/broker_manager.h include/log.h include/connection.h \
+ include/connection_zmq.h include/thirdparty/zhelpers.hpp \
+ include/thirdparty/zmq.hpp /opt/zeromq/include/zmq.h \
+ include/monitor_zmq.h include/thirdparty/readerwriterqueue.h \
+ include/thirdparty/atomicops.h include/thirdparty/zmq.hpp \
+ include/utils.h /usr/local/opt/zlib/include/zlib.h \
+ /usr/local/opt/zlib/include/zconf.h include/broker_config.h \
+ include/broker.h include/connection_file.h include/file_details.h \
+ include/producer.h include/broker_storage.h include/consumer.h \
+ include/connection_socket.h include/admin_cmd.h \
+ include/thirdparty/picojson.h
+
+include/log.h:
+
+include/thirdparty/spdlog/spdlog.h:
+
+include/thirdparty/spdlog/common.h:
+
+include/thirdparty/spdlog/logger.h:
+
+include/thirdparty/spdlog/sinks/base_sink.h:
+
+include/thirdparty/spdlog/sinks/./sink.h:
+
+include/thirdparty/spdlog/sinks/./../details/log_msg.h:
+
+include/thirdparty/spdlog/sinks/./../details/./format.h:
+
+include/thirdparty/spdlog/sinks/../formatter.h:
+
+include/thirdparty/spdlog/sinks/../details/pattern_formatter_impl.h:
+
+include/thirdparty/spdlog/sinks/../details/./os.h:
+
+include/thirdparty/spdlog/./details/logger_impl.h:
+
+include/thirdparty/spdlog/./details/./line_logger.h:
+
+include/thirdparty/spdlog/details/spdlog_impl.h:
+
+include/thirdparty/spdlog/details/registry.h:
+
+include/thirdparty/spdlog/details/../async_logger.h:
+
+include/thirdparty/spdlog/details/.././details/async_logger_impl.h:
+
+include/thirdparty/spdlog/details/.././details/./async_log_helper.h:
+
+include/thirdparty/spdlog/details/.././details/././mpmc_bounded_q.h:
+
+include/thirdparty/spdlog/details/.././details/././format.h:
+
+include/thirdparty/spdlog/details/../sinks/file_sinks.h:
+
+include/thirdparty/spdlog/details/../sinks/../details/null_mutex.h:
+
+include/thirdparty/spdlog/details/../sinks/../details/file_helper.h:
+
+include/thirdparty/spdlog/details/../sinks/../details/format.h:
+
+include/thirdparty/spdlog/details/../sinks/stdout_sinks.h:
+
+include/thirdparty/spdlog/details/../sinks/./ostream_sink.h:
+
+include/thirdparty/spdlog/details/../sinks/syslog_sink.h:
+
+include/broker_manager.h:
+
+include/log.h:
+
+include/connection.h:
+
+include/connection_zmq.h:
+
+include/thirdparty/zhelpers.hpp:
+
+include/thirdparty/zmq.hpp:
+
+/opt/zeromq/include/zmq.h:
+
+include/monitor_zmq.h:
+
+include/thirdparty/readerwriterqueue.h:
+
+include/thirdparty/atomicops.h:
+
+include/thirdparty/zmq.hpp:
+
+include/utils.h:
+
+/usr/local/opt/zlib/include/zlib.h:
+
+/usr/local/opt/zlib/include/zconf.h:
+
+include/broker_config.h:
+
+include/broker.h:
+
+include/connection_file.h:
+
+include/file_details.h:
+
+include/producer.h:
+
+include/broker_storage.h:
+
+include/consumer.h:
+
+include/connection_socket.h:
+
+include/admin_cmd.h:
+
+include/thirdparty/picojson.h:
