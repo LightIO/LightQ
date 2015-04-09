30772795b9e145c530ed16ad2feded26f069d40c
Rohit Joshi <rohit.c.joshi@gmail.com>
2015-04-08 23:57:05 -0400

    Added support for passing process name as part of the log file. using free_broker_mgr_conn to free the object.

--- examples/lightq-broker.c
+++ examples/lightq-broker.c
@@ -12,11 +12,12 @@
 #include <ctype.h>
 #include <string.h>
 #include "lightq_api.h"
+
 /*
  * start broker
  */
 int main(int argc, char** argv) {
-    
+
     int c;
     char *admin_userid = "lightq_admin";
     char *admin_password = "T0p$3cr31";
@@ -24,72 +25,72 @@ int main(int argc, char** argv) {
     unsigned bind_port = 5500;
     char *transport = "tcp";
     char *loglevel = "event";
-    
-   
-    while ((c = getopt (argc, argv, "hu:p:i:b:t:l:")) != -1) {
-    switch (c)
-      {
-      case 'h':
-        printf("Usage: [%s] [-u admin_userid[%s]] [-p admin_password[%s]] [-i bind_ip[%s]] [-b bind_port[%d]] [-t transport[%s]] [-l loglevel[event]]\n", 
-                argv[0], admin_userid, admin_password, bind_ip, bind_port, transport);
-        return 1;
-      case 'u':
-        admin_userid = optarg;
-        break;
-      case 'p':
-        admin_password = optarg;
-        break;
-      case 'i':
-        bind_ip = optarg;
-        break;
-     
-      case 'b':
-        bind_port = atoi(optarg);
-        if(bind_port == 0) {
-            printf("Invalid bindport 0\n");
-            return -1;
+
+
+    while ((c = getopt(argc, argv, "hu:p:i:b:t:l:")) != -1) {
+        switch (c) {
+            case 'h':
+                printf("Usage: [%s] [-u admin_userid[%s]] [-p admin_password[%s]] [-i bind_ip[%s]] [-b bind_port[%d]] [-t transport[%s]] [-l loglevel[event]]\n",
+                        argv[0], admin_userid, admin_password, bind_ip, bind_port, transport);
+                return 1;
+            case 'u':
+                admin_userid = optarg;
+                break;
+            case 'p':
+                admin_password = optarg;
+                break;
+            case 'i':
+                bind_ip = optarg;
+                break;
+
+            case 'b':
+                bind_port = atoi(optarg);
+                if (bind_port == 0) {
+                    printf("Invalid bindport 0\n");
+                    return -1;
+                }
+                break;
+
+            case 'l':
+                loglevel = optarg;
+                break;
+
+            case 't':
+                transport = optarg;
+                if ((strcmp("tcp", transport) != 0) && (strcmp("ipc", transport) != 0) && (strcmp("inproc", transport) != 0)) {
+                    printf("Invalid transport %s. Must be one of 'tcp', 'ipc', 'inproc\n", transport);
+                    return -1;
+                }
+                break;
+            case '?':
+                if (isprint(optopt))
+                    fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+                else
+                    fprintf(stderr,
+                        "Unknown option character `\\x%x'.\n",
+                        optopt);
+                return 1;
+            default:
+                break;
         }
-        break;
-        
-       case 'l':
-        loglevel = optarg;
-        break;
-      
-      case 't':
-        transport = optarg;
-        if((strcmp("tcp", transport) != 0) && (strcmp("ipc", transport) != 0) && (strcmp("inproc", transport) != 0)) {
-            printf("Invalid transport %s. Must be one of 'tcp', 'ipc', 'inproc\n");
-            return -1;
-        }
-        break;
-      case '?':
-        if (isprint (optopt))
-          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-        else
-          fprintf (stderr,
-                   "Unknown option character `\\x%x'.\n",
-                   optopt);
-        return 1;
-      default:
-        break;
-      }
     }
-    
-     printf("[-u admin_userid[%s]] [-p admin_password[%s]] [-i bind_ip[%s]] [-b bind_port[%d]] [-t transport[%s]] [-l loglevel[%s]]\n", 
-                argv[0], admin_userid, admin_password, bind_ip, bind_port, transport, logleve);
+
+    printf("process_name[%s], [-u admin_userid[%s]] [-p admin_password[%s]] [-i bind_ip[%s]] [-b bind_port[%d]] [-t transport[%s]] [-l loglevel[%s]]\n",
+            argv[0], admin_userid, admin_password, bind_ip, bind_port, transport, loglevel);
 
     lightq_loglevel level = str_to_loglevel(loglevel);
     init_log(argv[0], level);
-    
-    lightq_broker_mgr* p_broker =  init_broker(admin_userid, admin_password, transport, bind_ip, bind_port);
-    
-    if(p_broker) {
+
+    lightq_broker_mgr* p_broker = init_broker(admin_userid, admin_password, transport, bind_ip, bind_port);
+
+    if (p_broker) {
         run_broker(p_broker, true);
-    }else {
-      printf("Failed to initialize broker");
+    } else {
+        printf("Failed to initialize broker\n");
     }
-      
-   return (EXIT_SUCCESS);
-    }
+    free_broker_mgr(p_broker);
+
+    return (EXIT_SUCCESS);
+
 }
 
